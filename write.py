bdad684595d7b9ffee97fc9f4bdd2d6fd1a3b173
Michael Zhu <michaelwzhu@berkeley.edu>
2022-01-05 16:26:43 -0800

    256

--- write.py
+++ write.py
@@ -1,9 +1,9 @@
 lines = []
-start = 500
-stop = 635
+start = 0
+stop = 499
 
 for i in range(start, stop + 1):
-    lines.append('/content/taming-transformers/data/BAYC_test/' + str(i) + '.png')
+    lines.append('/content/taming-transformers/data/BAYC/' + str(i) + '.png')
 
-with open('data/BAYC_test.txt', 'w') as f:
+with open('data/BAYC_train.txt', 'w') as f:
     f.write('\n'.join(lines))
